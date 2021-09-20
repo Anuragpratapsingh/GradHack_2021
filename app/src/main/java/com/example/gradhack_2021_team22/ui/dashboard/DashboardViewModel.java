49216faf7f95bcc526f140a9d758ae3853da2f3e
Anuragpratapsingh <53467402+Anuragpratapsingh@users.noreply.github.com>
2021-09-20 14:46:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/gradhack_2021_team22/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.gradhack_2021_team22.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
